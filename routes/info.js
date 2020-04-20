0d8a3894c4f42e5f8aa7b9ef91ede4233214bd0c
kevinmaiden7 <kevin.m.gallego@gmail.com>
2020-04-20 14:23:20 -0500

    Adding changes: fixing a bug

--- routes/info.js
+++ routes/info.js
@@ -6,7 +6,6 @@ var pkg = require(path.join(__dirname, '../package.json'));
 /* GET users listing. */
 router.get('/info', function(req, res, next) {
   //res.render('text', { text: 'Getting a resource: JSON file' });
-  console.log("Accessing /info path");
   res.json({
     name: pkg.name,
     version: pkg.version,
